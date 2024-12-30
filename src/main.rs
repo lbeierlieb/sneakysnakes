f9d122deed8398f7303cf7c51297e08b214f56cb
Lukas Beierlieb <lukasbrlb.lb@gmail.com>
2024-12-30 20:47:23 +0100

    bevy: add main menu

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use bevy::{color::palettes::basic::*, prelude::*};
 
+#[derive(States, Debug, Clone, PartialEq, Eq, Hash)]
+enum AppState {
+    MainMenu,
+    InGame,
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -11,12 +17,36 @@ fn main() {
             }),
             ..default()
         }))
-        .add_systems(Startup, setup)
-        .add_systems(Update, game_logic)
+        .insert_state::<AppState>(AppState::MainMenu)
+        .add_systems(OnEnter(AppState::MainMenu), setup_main_menu)
+        .add_systems(OnExit(AppState::MainMenu), cleanup_main_menu)
+        .add_systems(OnEnter(AppState::InGame), setup_in_game)
+        .add_systems(OnExit(AppState::InGame), cleanup_in_game)
+        .add_systems(Update, update_main_menu.run_if(in_state(AppState::MainMenu)))
+        .add_systems(Update, game_logic.run_if(in_state(AppState::InGame)))
         .run();
 }
 
-fn setup(
+fn setup_main_menu(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Text2d::new("Press space to start"),
+    ));
+}
+
+fn cleanup_main_menu(mut commands: Commands, query: Query<Entity, Or<(With<Camera>, With<Text2d>)>>) {
+    for entity in &query {
+        commands.entity(entity).despawn();
+    }
+}
+
+fn update_main_menu(mut commands: Commands, keyboard_input: Res<ButtonInput<KeyCode>>) {
+    if keyboard_input.pressed(KeyCode::Space) {
+        commands.set_state(AppState::InGame);
+    }
+}
+
+fn setup_in_game (
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
@@ -31,6 +61,12 @@ fn setup(
         MeshMaterial2d(materials.add(Color::from(YELLOW))),
         Transform::default().with_scale(Vec3::splat(5.)).with_translation(Vec3::new(10.,10.,0.)),
     ));
+}
+
+fn cleanup_in_game(mut commands: Commands, query: Query<Entity, Or<(With<Camera>, With<Mesh2d>)>>) {
+    for entity in &query {
+        commands.entity(entity).despawn();
+    }
 }
 
 #[derive(Component)]
@@ -58,6 +94,8 @@ fn game_logic(mut query: Query<(&mut Transform, &mut Player)>, keyboard_input: R
         } else if keyboard_input.pressed(KeyCode::ArrowRight) {
             let rotation = Quat::from_rotation_z(-std::f32::consts::PI / 30.0);
             player.dir = rotation.mul_vec3(player.dir);
+        } else if keyboard_input.pressed(KeyCode::Escape) {
+            commands.set_state(AppState::MainMenu);
         }
         transform.translation += player.dir * time.delta_secs() * player.speed;
         commands.spawn((
